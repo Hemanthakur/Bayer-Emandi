8a4ca18cbc37bb3c0d46e77078e6ec1393faa21c
Hemant <108360260+Hemanthakur@users.noreply.github.com>
2022-11-28 16:56:02 +0530

    Update RegistrationServiceImpl.java

--- Ecommerce/Ecommerce/src/main/java/com/example/service/Impl/RegistrationServiceImpl.java
+++ Ecommerce/Ecommerce/src/main/java/com/example/service/Impl/RegistrationServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.model.Seller;
 import com.example.repo.SellerRepository;
+import com.example.request.SellerRequest;
 import com.example.service.RegistrationService;
 
 @Service
@@ -13,24 +14,12 @@ public class RegistrationServiceImpl implements RegistrationService{
 	@Autowired
 	private SellerRepository sellerRepo;
 	
-//	public Seller createUser(Seller seller) throws Exception {
-//		Seller user=sellerRepo.findByPhoneNumber(seller.getPhoneNumber());
-//		if(user!=null) {
-//			System.out.println("Seller already Exist");
-//			throw new Exception("User Already Exist");
-//		}else {
-//			user=sellerRepo.save(seller);
-//		}
-//		
-//		return user; 	
-//	}
 	
-	public Seller createUser(Seller seller){
-		Seller user=sellerRepo.save(seller);		
+	public Seller createUserReq(SellerRequest sellerRequest){
+		Seller user=new Seller(sellerRequest);
+		 user=sellerRepo.save(user);		
 		return user; 	
 	}
-	
-	
 	
 	
 	public Seller  findByPhoneNumber(String phoneNumber) {
